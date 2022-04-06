8886447aad08cfc35a58edb56f8312174171939f
seubpong.mon <pjame.fb@gmail.com>
2022-04-06 17:54:47 +0700

    Added CRDs

--- 03-1-deploy-runner-controller.bash
+++ 03-1-deploy-runner-controller.bash
@@ -2,7 +2,16 @@
 
 source .env
 
+CRD_URL=https://raw.githubusercontent.com/actions-runner-controller/actions-runner-controller/v0.22.2/charts/actions-runner-controller/crds/
 kubectl create ns ${NS1}
+
+
+kubectl apply -n ${NS1} -f ${CRD_URL}/actions.summerwind.dev_horizontalrunnerautoscalers.yaml
+kubectl apply -n ${NS1} -f ${CRD_URL}/actions.summerwind.dev_runnerdeployments.yaml
+kubectl apply -n ${NS1} -f ${CRD_URL}/actions.summerwind.dev_runnerreplicasets.yaml
+kubectl apply -n ${NS1} -f ${CRD_URL}/actions.summerwind.dev_runners.yaml
+kubectl apply -n ${NS1} -f ${CRD_URL}/actions.summerwind.dev_runnersets.yaml
+
 
 helm repo add runner-controller https://actions-runner-controller.github.io/actions-runner-controller
 
